9be321afb6559954d91026321ef7601401e4b6d2
jyyang5 <jyyang55@outlook.com>
2018-02-06 00:38:51 -0400

    validate
    CSCI 3130 a2-stage2

--- Validator.java
+++ Validator.java
@@ -5,10 +5,16 @@
 public class Validator {
 
      int validate(String inputPassword){
-            int countCorrectRule = 0;                 // count for correct rules
-            if (!inputPassword.equals( "password"))   // rule 1: check if password is "password"
+            int countCorrectRule = 0;                               // count for correct rules
+            if (!inputPassword.equals( "password"))                 // rule 1: check if password is "password"
                 countCorrectRule++;
-            if(inputPassword.length() >= 7)           //rule 2: check if length of password >= 8
+            if(inputPassword.length() >= 7)                         //rule 2: check if length of password >= 8
+                countCorrectRule++;
+            if(!inputPassword.equals(inputPassword.toLowerCase()))  //rule 3: check if contains an upper case letter
+                countCorrectRule++;
+            if(!inputPassword.equals(inputPassword.toUpperCase()))  //rule 4: check if contains a lower case letter
+                countCorrectRule++;
+            if(inputPassword.matches(".*\\d+.*"))                   //rule 5: check if contains a digit
                 countCorrectRule++;
             return countCorrectRule;
         }
